9fd43b634bc31163be725aa90841cfe904e4e4b5
Jozef Piecyk <jozollo@gmail.com>
2018-11-11 19:47:59 +0100

    add context replacement and dynamic import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import format from 'date-fns/format';
 import subDays from 'date-fns/sub_days';
-import pl from 'date-fns/locale/pl';
+const getLocale = locale => import(`date-fns/locale/${locale}/index.js`);
+
 let now = new Date();
 
 console.log(`dzisiaj jest ${now}`);
@@ -8,15 +9,19 @@ console.log(`dzisiaj jest ${now}`);
 now = format(now, 'D-M-YYYY');
 console.log(`ładniej sformatowane ${now}`);
 
-// now = moment().format('dddd D MMMM Y');
-now = format(new Date(), 'dddd D MMMM YYYY', {
-  locale: pl
-});
-console.log(`Albo po polsku -  ${now}`);
+let pl = {};
+getLocale('pl').then(module => {
+  pl = module.default;
 
-const yesterday = subDays(new Date(), 1);
-console.log(
-  `Wczoraj był ${format(yesterday, 'dddd D MMMM YYYY', {
+  now = format(new Date(), 'dddd D MMMM YYYY', {
     locale: pl
-  })}`
-);
+  });
+  console.log(`Albo po polsku -  ${now}`);
+
+  const yesterday = subDays(new Date(), 1);
+  console.log(
+    `Wczoraj był ${format(yesterday, 'dddd D MMMM YYYY', {
+      locale: pl
+    })}`
+  );
+});
